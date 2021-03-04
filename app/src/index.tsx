a34a0cbf1bf50cdb400161c0c4e7844685427632
Romain Fromi <romain.fromi@gmail.com>
2021-03-04 10:16:53 +0100

    Init Game Park react-client

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,15 @@
+import MyBoardGame from '@gamepark/board-game-template'
+import {createGameStore} from '@gamepark/react-client'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 import App from './App'
-import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Provider store={createGameStore('board-game-template', MyBoardGame)}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
