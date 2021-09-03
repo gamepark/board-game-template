64fc74b43267be0f8617e8454d30903fda503452
Romain Fromi <romain.fromi@gmail.com>
2021-09-03 15:58:02 +0200

    Upgrade dependencies

--- app/src/HeaderText.tsx
+++ app/src/HeaderText.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import GameView from '@gamepark/board-game-template/GameView'
+import {getPlayerName} from '@gamepark/board-game-template/MyBoardGameOptions'
 import {usePlayerId} from '@gamepark/react-client'
 import {useTranslation} from 'react-i18next'
 
@@ -12,5 +13,5 @@ export default function HeaderText({loading}: Props) {
   const {t} = useTranslation()
   const playerId = usePlayerId()
   if (loading) return <>{t('Game loading...')}</>
-  return <>Loaded! Now what? Your player id is {playerId}</>
+  return <>Loaded! Now what? Your player id is {getPlayerName(playerId, t)}</>
 }
