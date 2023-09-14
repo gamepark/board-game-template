4389e01ac9e9b763652732c807f2843295ddf764
Romain Fromi <romain.fromi@gmail.com>
2023-09-14 10:44:51 +0200

    Upgrade framework

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,17 +1,8 @@
-const {override, addWebpackPlugin} = require('customize-cra')
+const { override, addWebpackPlugin } = require('customize-cra')
 const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin')
-const {aliasDangerous, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous')
+const { aliasDangerous, configPaths } = require('react-app-rewire-alias/lib/aliasDangerous')
 
 module.exports = (config, env) => {
-  config.module.rules.splice(0, 0, {
-    test: /\.worker\.(js|ts)$/i,
-    use: [{
-      loader: 'comlink-loader',
-      options: {
-        singleton: true
-      }
-    }]
-  })
   return override(
     addWebpackPlugin(new ImageminWebpWebpackPlugin()),
     aliasDangerous(configPaths('./tsconfig.paths.json'))
