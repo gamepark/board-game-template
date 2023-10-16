dee934d55588880e1fade4aab44e89ed033486b4
Romain Fromi <romain.fromi@gmail.com>
2023-10-16 17:33:39 +0200

    Upgrade framework

--- rules/src/GameTemplateSetup.ts
+++ rules/src/GameTemplateSetup.ts
@@ -1,5 +1,6 @@
 import { MaterialGameSetup } from '@gamepark/rules-api'
 import { GameTemplateOptions } from './GameTemplateOptions'
+import { GameTemplateRules } from './GameTemplateRules'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
 import { PlayerColor } from './PlayerColor'
@@ -9,10 +10,12 @@ import { RuleId } from './rules/RuleId'
  * This class creates a new Game based on the game options
  */
 export class GameTemplateSetup extends MaterialGameSetup<PlayerColor, MaterialType, LocationType, GameTemplateOptions> {
+  Rules = GameTemplateRules
+
   setupMaterial(_options: GameTemplateOptions) {
   }
 
   start() {
-    return { id: RuleId.PlayerTurn, player: this.game.players[0] }
+    this.startPlayerTurn(RuleId.PlayerTurn, this.game.players[0])
   }
 }
