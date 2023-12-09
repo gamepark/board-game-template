e3fce76fba5fbe9a11cbb76556e825649824a046
Patrick Beaume <p.beaume@gmail.com>
2023-12-09 17:21:26 +0100

    don't display the GameDisplay if game is not

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@
 import { FailuresDialog, FullscreenDialog, LoadingScreen, MaterialHeader, Menu, useGame } from '@gamepark/react-game'
 import { MaterialGame } from '@gamepark/rules-api'
 import { useEffect, useState } from 'react'
-import GameDisplay from './GameDisplay'
+import { GameDisplay } from './GameDisplay'
 import { Headers } from './headers/Headers'
 
 export default function App() {
@@ -14,7 +14,7 @@ export default function App() {
   const loading = !game || isJustDisplayed
   return (
     <>
-      <GameDisplay/>
+      { !!game && <GameDisplay players={game.players.length} /> }
       <LoadingScreen display={loading} author="Someone" artist="Somebody" publisher="Nobody" developer="You"/>
       <MaterialHeader rulesStepsHeaders={Headers} loading={loading}/>
       <Menu/>
--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,8 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { GameTable } from '@gamepark/react-game'
+import { FC } from 'react'
 import { PlayerPanels } from './panels/PlayerPanels'
 
-export default function GameDisplay() {
+type GameDisplayProps = {
+  players: number
+}
+
+export const GameDisplay: FC<GameDisplayProps> = () => {
   return <>
     <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30}
                margin={{ top: 7, left: 0, right: 30, bottom: 0 }}/>
