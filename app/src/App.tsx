5171045e3d56a1937cd3aac839484d135e3bf3ce
Patrick Beaume <p.beaume@gmail.com>
2024-03-15 10:37:31 +0100

    add MaterialImageLoader by default

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { FailuresDialog, FullscreenDialog, LoadingScreen, MaterialHeader, Menu, useGame } from '@gamepark/react-game'
+import { FailuresDialog, FullscreenDialog, LoadingScreen, MaterialHeader, MaterialImageLoader, Menu, useGame } from '@gamepark/react-game'
 import { MaterialGame } from '@gamepark/rules-api'
 import { useEffect, useState } from 'react'
 import { GameDisplay } from './GameDisplay'
@@ -8,15 +8,17 @@ import { Headers } from './headers/Headers'
 export default function App() {
   const game = useGame<MaterialGame>()
   const [isJustDisplayed, setJustDisplayed] = useState(true)
+  const [isImagesLoading, setImagesLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => setJustDisplayed(false), 2000)
   }, [])
-  const loading = !game || isJustDisplayed
+  const loading = !game || isJustDisplayed || isImagesLoading
   return (
     <>
       { !!game && <GameDisplay players={game.players.length} /> }
       <LoadingScreen display={loading} author="Someone" artist="Somebody" publisher="Nobody" developer="You"/>
       <MaterialHeader rulesStepsHeaders={Headers} loading={loading}/>
+      <MaterialImageLoader onImagesLoad={() => setImagesLoading(false)} />
       <Menu/>
       <FailuresDialog/>
       <FullscreenDialog/>
