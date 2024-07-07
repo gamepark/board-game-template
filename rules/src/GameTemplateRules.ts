1127b8522a5fa3daf75a2786118c84f13d718428
Patrick Beaume <p.beaume@gmail.com>
2024-07-07 18:28:20 +0200

    Add default TimeLimit implementation to prevent forgetting it

--- rules/src/GameTemplateRules.ts
+++ rules/src/GameTemplateRules.ts
@@ -1,4 +1,4 @@
-import { MaterialRules } from '@gamepark/rules-api'
+import { MaterialGame, MaterialMove, MaterialRules, TimeLimit } from '@gamepark/rules-api'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
 import { PlayerColor } from './PlayerColor'
@@ -10,8 +10,13 @@ import { RuleId } from './rules/RuleId'
  * This class implements the rules of the board game.
  * It must follow Game Park "Rules" API so that the Game Park server can enforce the rules.
  */
-export class GameTemplateRules extends MaterialRules<PlayerColor, MaterialType, LocationType> {
+export class GameTemplateRules extends MaterialRules<PlayerColor, MaterialType, LocationType>
+  implements TimeLimit<MaterialGame<PlayerColor, MaterialType, LocationType>, MaterialMove<PlayerColor, MaterialType, LocationType>, PlayerColor> {
   rules = {
     [RuleId.PlayerTurn]: PlayerTurn
+  }
+
+  giveTime(): number {
+    return 60
   }
 }
