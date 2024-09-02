e0121ffcbaf3988a7e17d46b0553119445feb452
Romain Fromi <romain.fromi@gmail.com>
2024-09-02 09:52:44 +0200

    Table 1px white border in dev

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,4 +1,5 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { GameTable, GameTableNavigation } from '@gamepark/react-game'
 import { FC } from 'react'
 import { PlayerPanels } from './panels/PlayerPanels'
@@ -10,7 +11,8 @@ type GameDisplayProps = {
 export const GameDisplay: FC<GameDisplayProps> = () => {
   return <>
     <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30}
-               margin={{ top: 7, left: 0, right: 30, bottom: 0 }}>
+               margin={{ top: 7, left: 0, right: 30, bottom: 0 }}
+               css={process.env.NODE_ENV === 'development' && css`border: 1px solid white;`}>
       <GameTableNavigation/>
       <PlayerPanels/>
     </GameTable>
