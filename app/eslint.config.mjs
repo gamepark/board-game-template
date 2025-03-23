d2dc4e5158b8ec9db7ba15f0d5074f9397e4edd2
Romain Fromi <romain.fromi@gmail.com>
2025-03-23 22:05:33 +0100

    Allow unsafe-enum-comparison

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -3,20 +3,20 @@ import pluginJs from '@eslint/js'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
-import {globalIgnores} from 'eslint/config'
+import { globalIgnores } from 'eslint/config'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config([
-    globalIgnores(["build/**/*.js", "config-overrides.js", "eslint.config.mjs"]),
+    globalIgnores(['build/**/*.js', 'config-overrides.js', 'eslint.config.mjs']),
     eslint.configs.recommended,
     tseslint.configs.strictTypeChecked,
     tseslint.configs.stylisticTypeChecked,
     {
-        files: ["src/**/*.{js,mjs,ts,jsx,tsx}"],
+        files: ['src/**/*.{js,mjs,ts,jsx,tsx}'],
         ...pluginJs.configs.recommended,
         settings: {
             react: {
-                version: "17.0",
+                version: '17.0'
             }
         },
         ...pluginReact.configs.flat.recommended,
@@ -26,18 +26,19 @@ export default tseslint.config([
                 projectService: true,
                 tsconfigRootDir: import.meta.dirname,
                 ecmaFeatures: {
-                    jsx: true,
-                },
+                    jsx: true
+                }
             },
             globals: globals.browser
         },
         rules: {
-            "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-            "@typescript-eslint/restrict-template-expressions": ["error", { "allowNullish": true, "allowNumber": true }],
-            "@typescript-eslint/no-confusing-void-expression": "off",
-            "@typescript-eslint/consistent-type-definitions": "off",
-            "@typescript-eslint/no-non-null-assertion": "off"
+            '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+            '@typescript-eslint/restrict-template-expressions': ['error', { 'allowNullish': true, 'allowNumber': true }],
+            '@typescript-eslint/no-confusing-void-expression': 'off',
+            '@typescript-eslint/consistent-type-definitions': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-unsafe-enum-comparison': 'off'
         }
     },
-    eslintPluginPrettierRecommended,
+    eslintPluginPrettierRecommended
 ])
