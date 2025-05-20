209afa3ae28d2c869a1c327239db3f97c94a8417
Patrick Beaume <p.beaume@gmail.com>
2025-05-20 15:16:38 +0200

    Add MaterialGameSounds

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import { FailuresDialog, FullscreenDialog, LoadingScreen, MaterialHeader, MaterialImageLoader, Menu, useGame } from '@gamepark/react-game'
+import { FailuresDialog, FullscreenDialog, LoadingScreen, MaterialGameSounds, MaterialHeader, MaterialImageLoader, Menu, useGame } from '@gamepark/react-game'
+import { MaterialSoundLoader } from '@gamepark/react-game/dist/components/material/sound/MaterialSoundLoader'
 import { MaterialGame } from '@gamepark/rules-api'
 import { useEffect, useState } from 'react'
 import { GameDisplay } from './GameDisplay'
@@ -9,16 +10,18 @@ export default function App() {
   const game = useGame<MaterialGame>()
   const [isJustDisplayed, setJustDisplayed] = useState(true)
   const [isImagesLoading, setImagesLoading] = useState(true)
+  const [isSoundLoading, setSoundsLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => setJustDisplayed(false), 2000)
   }, [])
-  const loading = !game || isJustDisplayed || isImagesLoading
+  const loading = !game || isJustDisplayed || isImagesLoading || isSoundLoading
   return (
     <>
       {!!game && <GameDisplay players={game.players.length} />}
       <LoadingScreen display={loading} author="Someone" artist="Somebody" publisher="Nobody" developer="You" />
       <MaterialHeader rulesStepsHeaders={Headers} loading={loading} />
       <MaterialImageLoader onImagesLoad={() => setImagesLoading(false)} />
+      <MaterialGameSounds onSoundsLoad={() => setSoundsLoading(false)} />
       <Menu />
       <FailuresDialog />
       <FullscreenDialog />
