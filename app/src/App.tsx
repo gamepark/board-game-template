4bd45e93214e5e50b9acb0cfa13d872d53981ecd
Patrick Beaume <p.beaume@gmail.com>
2025-05-22 12:52:14 +0200

    Don't display loader during sound loading

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { FailuresDialog, FullscreenDialog, LoadingScreen, MaterialGameSounds, MaterialHeader, MaterialImageLoader, Menu, useGame } from '@gamepark/react-game'
-import { MaterialSoundLoader } from '@gamepark/react-game/dist/components/material/sound/MaterialSoundLoader'
 import { MaterialGame } from '@gamepark/rules-api'
 import { useEffect, useState } from 'react'
 import { GameDisplay } from './GameDisplay'
@@ -10,18 +9,17 @@ export default function App() {
   const game = useGame<MaterialGame>()
   const [isJustDisplayed, setJustDisplayed] = useState(true)
   const [isImagesLoading, setImagesLoading] = useState(true)
-  const [isSoundLoading, setSoundsLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => setJustDisplayed(false), 2000)
   }, [])
-  const loading = !game || isJustDisplayed || isImagesLoading || isSoundLoading
+  const loading = !game || isJustDisplayed || isImagesLoading
   return (
     <>
       {!!game && <GameDisplay players={game.players.length} />}
       <LoadingScreen display={loading} author="Someone" artist="Somebody" publisher="Nobody" developer="You" />
       <MaterialHeader rulesStepsHeaders={Headers} loading={loading} />
       <MaterialImageLoader onImagesLoad={() => setImagesLoading(false)} />
-      <MaterialGameSounds onSoundsLoad={() => setSoundsLoading(false)} />
+      <MaterialGameSounds />
       <Menu />
       <FailuresDialog />
       <FullscreenDialog />
