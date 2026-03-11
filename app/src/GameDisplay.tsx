a9118f292f7f788df5ae91c044a1c5b6c0b6d96c
Patrick Beaume <p.beaume@gmail.com>
2026-03-11 16:27:10 +0100

    Add DevToolsHub to template GameDisplay

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react'
-import { GameTable, GameTableNavigation } from '@gamepark/react-game'
+import { DevToolsHub, GameTable, GameTableNavigation } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 
 export function GameDisplay() {
@@ -9,6 +9,7 @@ export function GameDisplay() {
       <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
         <GameTableNavigation />
         <PlayerPanels />
+        {process.env.NODE_ENV === 'development' && <DevToolsHub />}
       </GameTable>
     </>
   )
